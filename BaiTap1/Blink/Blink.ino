6aaf0907f2b74b7c0a27c70d5675b09d00fe5525
Chí Thành <91357676+thanh057hdq@users.noreply.github.com>
2021-09-29 16:56:57 +0700

    Update Blink.ino

--- BaiTap1/Blink/Blink.ino
+++ BaiTap1/Blink/Blink.ino
@@ -1,17 +1,7 @@
-int x = 0;
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  x = digitalRead(2);
-
-  if( x == HIGH) {
-    digitalWrite(13, HIGH);
-  } else {
-    digitalWrite(13, LOW);
-  }
+  digitalWrite(13,HIGH);
 }
